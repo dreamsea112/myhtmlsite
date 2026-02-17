d1578a46f2925b204f3031461b50a55d9ce68cff
dreamsea112 <dreamsea112@gmail.com>
2026-02-17 09:45:23 +0800

    Add files via upload

---
+++ System-testing/service-worker.js
@@ -0,0 +1,31 @@
+﻿// PWA 快取版本號
+const CACHE_NAME = 'bp-system-v1';
+
+// 要快取的檔案
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安裝時快取
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截請求
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
